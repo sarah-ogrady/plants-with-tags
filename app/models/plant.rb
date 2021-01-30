c0c03a09c845a5da15e6b94055cafe4b70dca48a
Sarah OGrady <sarahfrancesogrady@gmail.com>
2021-01-30 17:47:30 +0000

    finished livecode

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,5 +1,13 @@
 class Plant < ApplicationRecord
   belongs_to :garden
-
+  has_many :plant_tags, dependent: :destroy
+  has_many :tags, through: :plant_tags
   validates :name, :image_url, presence: true
 end
+
+
+# how do we make our tags table
+
+# generate a tag model
+# generate plant tags model
+# routes
